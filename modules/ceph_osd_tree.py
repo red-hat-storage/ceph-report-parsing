11b57adf347cfd456711d10fb74def3ae25a6fc1
Michael J. Kidd <lk@orion.rv>
2016-08-11 19:01:57 -0700

    Updated to not bomb on discrepancy between crushmap:buckets:items and osdmap:osds

--- modules/ceph_osd_tree.py
+++ modules/ceph_osd_tree.py
@@ -22,23 +22,24 @@ def dump_items(b):
    for i in b['items']:
       index = 0
       if i['id'] > -1:
-         outline = "{0:6d}\t".format(i['id'])
-         outline += "{0:8.4}\t".format(float(i['weight'])*0.00001526)
-         while index < dump_bucket_type:
-            outline += '\t'
-            index += 1
-         outline += "osd."+str(i['id'])+"\t\t\t   "
-         if osdById[i['id']]['up']  == 1:
-            outline += "up/"
-         else:
-            outline += "down/"
-         if osdById[i['id']]['in']  == 1:
-            outline += "in\t"
-         else:
-            outline += "out\t"
-         if 'weight' in osdById[i['id']]:
-            outline += "{0:4.3}".format(osdById[i['id']]['weight'])
-         print outline
+         if i['id'] in osdById:
+            outline = "{0:6d}\t".format(i['id'])
+            outline += "{0:8.4}\t".format(float(i['weight'])*0.00001526)
+            while index < dump_bucket_type:
+               outline += '\t'
+               index += 1
+            outline += "osd."+str(i['id'])+"\t\t\t   "
+            if osdById[i['id']]['up']  == 1:
+               outline += "up/"
+            else:
+               outline += "down/"
+            if osdById[i['id']]['in']  == 1:
+               outline += "in\t"
+            else:
+               outline += "out\t"
+            if 'weight' in osdById[i['id']]:
+               outline += "{0:4.3}".format(osdById[i['id']]['weight'])
+            print outline
       else:
          cb = bucketById[i['id']]
          outline = "{0:6d}\t".format(cb['id'])
